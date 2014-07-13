9c3118179cad86b5801deaf962143d260bb4159e
Marat Abdullin <dakota@brokenpipe.ru>
2014-07-13 23:00:32 +0400

    Beginning with input.

---
+++ src/input/input.js
@@ -0,0 +1,24 @@
+/* global Nya */
+/* global $B */
+'use strict';
+
+Nya.Input = Nya.extend({
+    init: function(node, inputNode) {
+        this._n = node || inputNode;
+        this._i = inputNode;
+    },
+
+    val: function(val) {
+        var ret = $B(this._i).attr('value', val);
+        return val === undefined ? ret : this;
+    },
+
+    disabled: function(val) {
+        var ret = $B(this._i).attr('disabled', val);
+        if (val !== undefined) {
+            $B(this._n).toggleClass('nya-input_disabled', !!val);
+            ret = this;
+        }
+        return ret;
+    }
+});
