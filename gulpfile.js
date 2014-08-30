bc8ebed226fe5bf6936c60ef5f5359f06f79e42c
Marat Abdullin <dakota@brokenpipe.ru>
2014-08-31 02:21:33 +0400

    Globally redo (nothing works yet).

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@
 var gulp = require('gulp');
 
 var eslint = require('gulp-eslint');
-var qunit = require('gulp-qunit');
-var stylus = require('gulp-stylus');
-var filter = require('gulp-filter');
-var add = require('gulp-add');
 var prefix = require('gulp-autoprefixer');
+var less = require('gulp-less');
+var add = require('gulp-add');
+
 var fs = require('fs');
+var path = require('path');
+
+// TODO: Replace with require.resolve() as soon as Bootstrap will release a fix.
+var BOOTSTRAP_PATH = '.';
+var DEST = 'nya';
 
 
 gulp.task('eslint', function() {
@@ -28,26 +32,44 @@ gulp.task('eslint', function() {
 });
 
 
-gulp.task('build', function() {
-    var stylFilter = filter('**/*.styl');
-    var cssFilter = filter(['**/*.css']);
+// Copy Bootstrap variables and mixins. To be able to include them in projects.
+gulp.task('copy-bootstrap', function() {
+    return gulp
+        .src([
+            path.join(BOOTSTRAP_PATH, 'node_modules/bootstrap/less/variables.less'),
+            path.join(BOOTSTRAP_PATH, 'node_modules/bootstrap/less/mixins.less'),
+            path.join(BOOTSTRAP_PATH, 'node_modules/bootstrap/less/**/mixins/*.less')
+        ])
+        .pipe(gulp.dest(path.join(DEST, '_bootstrap')));
+});
 
-    return gulp.src(['src/*/**/*.styl', '!src/*/**/_*.styl', 'src/**/*.js', 'src/**/*.ctpl', 'src/nya.css'])
-        .pipe(stylFilter)
-        .pipe(stylus({errors: true}))
-        .pipe(stylFilter.restore())
-        .pipe(cssFilter)
-        .pipe(prefix('last 1 version', '> 1%'))
-        .pipe(cssFilter.restore())
+
+// Copy .less files with variables.
+gulp.task('copy-less', function() {
+    gulp.src(['src/dark.less', 'src/light.less'])
+        .pipe(gulp.dest(DEST));
+});
+
+
+gulp.task('copy-templates', function() {
+    gulp.src('src/**/*.ctpl')
+        .pipe(gulp.dest(DEST));
+});
+
+
+gulp.task('copy-scripts', function() {
+    gulp.src('src/**/*.js')
         .pipe(add('babydom.js', fs.readFileSync(require.resolve('babydom'), {encoding: 'utf8'})))
-        .pipe(gulp.dest('./nya'));
+        .pipe(gulp.dest(DEST));
 });
 
 
-gulp.task('qunit', ['build'], function() {
-    //return gulp.src('./test/*.html')
-    //    .pipe(qunit());
+gulp.task('build-less', ['copy-bootstrap', 'copy-templates', 'copy-less', 'copy-scripts'], function() {
+    return gulp.src(['src/nya.light.less', 'src/nya.dark.less', 'src/*/**/*.less'])
+        .pipe(less({paths: [BOOTSTRAP_PATH, DEST]}))
+        .pipe(prefix('last 1 version', '> 1%'))
+        .pipe(gulp.dest(DEST));
 });
 
 
-gulp.task('default', ['eslint', 'qunit']);
+gulp.task('default', ['eslint', 'build-less']);
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var glob = require('glob');
+var glob = require('glob'),
+    path = require('path');
 
 module.exports = {
     BASE: __dirname,
-    FILES: glob.sync('./nya/**/*.ctpl', {cwd: __dirname})
+    FILES: glob.sync(path.join('nya', '**', '*.ctpl'), {cwd: __dirname}),
+    LESS: {
+        light: path.join(__dirname, 'nya', 'light.less'),
+        dark: path.join(__dirname, 'nya', 'dark.less')
+    }
 };
