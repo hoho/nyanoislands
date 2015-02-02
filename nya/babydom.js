40a9b627dec0e1788e7062a5ecaffb6e4a2e7759
Marat Abdullin <dakota@brokenpipe.ru>
2015-02-02 19:03:13 +0300

    Do not forget to build.

--- nya/babydom.js
+++ nya/babydom.js
@@ -1,5 +1,5 @@
 /*!
- * babydom v0.0.5, https://github.com/hoho/babydom
+ * babydom v0.0.6, https://github.com/hoho/babydom
  * (c) 2014 Marat Abdullin, MIT license
  */
 var $B = (function(document, encodeURIComponent, undefined) {
@@ -66,6 +66,9 @@ var $B = (function(document, encodeURIComponent, undefined) {
 
         while (node) {
             if ((($b = node.$b)) && ((h = $b[e.type]))) {
+                // Make a copy of current handlers (because the original list
+                // might change during callbacks execution).
+                h = h.slice(0);
                 for (i = 0; i < h.length; i++) {
                     h[i].call(node, e);
                     if (next & 2) { return; }
@@ -199,7 +202,7 @@ var $B = (function(document, encodeURIComponent, undefined) {
                 // TODO: It would probably be needed to distinguish event types.
                 //       For now we're not trying to follow standards much.
                 e = document.createEvent('HTMLEvents');
-                e.initEvent(event, true, false);
+                e.initEvent(event, true, true);
                 if (detail) {
                     e.detail = detail;
                 }
